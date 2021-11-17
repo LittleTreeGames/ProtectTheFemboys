64c1f3d99b47fb380edd3770965237dc5ea3729b
Lee <68012181+TheRealLoneLee@users.noreply.github.com>
2021-11-17 06:53:31 +0000

    Updated Camera + Movement Controller
    Updated the Camera + Movement scripts to allow for full third person camera that orbits around the character, and movement that makes the character move in the direction the camera is facing for much smoother movement ( Working on re adding gravity + jump and sprint features)

--- Main Unity Project/ProtectTheFemboys/Assets/Scripts/PlayerMovementScript.cs
+++ Main Unity Project/ProtectTheFemboys/Assets/Scripts/PlayerMovementScript.cs
@@ -7,75 +7,28 @@ public class PlayerMovementScript : MonoBehaviour
 {
     public CharacterController controller;
 
-    public float speed = 12f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
-    public Transform groundCheck;
-    public float groundDistance = 0.1f;
-    public LayerMask groundmask;
+    public Transform cam;
 
-    public Transform player;
-    Vector3 velocity;
-    bool isGrounded;
+    public float speed = 6f;
 
-
-    // Camera Shit
-    public Transform BoomArm;
-    public float mouseSensitivity = 100f;
-    float xRotation = 0f;
-
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
-
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    //update is called every frame
     void Update()
     {
-        // Actual Movement
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundmask);
-
-        if(isGrounded && velocity.y < 0)
+        if (direction.magnitude >= 0.1f)
         {
-            velocity.y = -2f;
+            
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            //move player
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir * speed * Time.deltaTime);
         }
-
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); 
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
-
-
-
-
-
-        /* Camera Movement        
-
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-        xRotation -= mouseY;
-
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        BoomArm.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-        // Rotate Player on Y axis
-        transform.Rotate(Vector3.up * mouseX);
-
-        // Rotate Camera Boom on X axis
-        BoomArm.Rotate(Vector3.right * -mouseY);
-
-        */
     }
 }
